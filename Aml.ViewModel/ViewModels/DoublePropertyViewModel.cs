223b9d7e85a910f4a7fd643179cdf17af941d0c1
Matthias Bartelt <matthias@bartelt-net.com>
2019-06-19 14:11:01 +0200

    DefaultValue property changed.

--- Aml.ViewModel/ViewModels/DoublePropertyViewModel.cs
+++ Aml.ViewModel/ViewModels/DoublePropertyViewModel.cs
@@ -5,42 +5,43 @@ using Aml.Engine.CAEX;
 
 namespace Aml.ViewModel
 {
-    public class DoublePropertyViewModel : BasePropertyViewModel<double>
-    {
-	    public sealed override double Value
-	    {
-		    get
-		    {
-			    var value = _attribute.Value;
-			    return value == null ? default(double) : double.Parse(_attribute.Value, CultureInfo.InvariantCulture);
-		    }
-		    set => _attribute.Value = Convert.ToString(value, CultureInfo.InvariantCulture);
-	    }
+	public class DoublePropertyViewModel : BasePropertyViewModel<double>
+	{
+		public sealed override double Value
+		{
+			get
+			{
+				var value = _attribute.Value;
+				return value == null ? default(double) : double.Parse(_attribute.Value, CultureInfo.InvariantCulture);
+			}
+			set => _attribute.Value = Convert.ToString(value, CultureInfo.InvariantCulture);
+		}
 
-	    public double? DefaultValue
-	    {
-		    get
-		    {
-			    var value = _attribute.DefaultValue;
-			    if (value == null) return null;
-			    return double.Parse(value, CultureInfo.InvariantCulture);
-		    }
-		    set => _attribute.DefaultValue = Convert.ToString(value, CultureInfo.InvariantCulture);
-	    }
+		public double? DefaultValue
+		{
+			get
+			{
+				var value = _attribute?.DefaultValue;
+				if (value == null) return null;
+				return Convert.ToDouble(value, CultureInfo.InvariantCulture);
+				//return double.Parse(value, CultureInfo.InvariantCulture);
+			}
+			set => _attribute.DefaultValue = Convert.ToString(value, CultureInfo.InvariantCulture);
+		}
 
 		public DoublePropertyViewModel(IAmlProvider provider)
 			: base(provider)
-	    { }
+		{ }
 
-	    public DoublePropertyViewModel(AttributeType model, IAmlProvider provider)
+		public DoublePropertyViewModel(AttributeType model, IAmlProvider provider)
 			: base(model, provider)
-	    { }
+		{ }
 
 		[Obsolete]
-        public DoublePropertyViewModel(string name, double typedValue, string unit, IAmlProvider provider)
-            : base(name, unit, provider)
-        {
-            Value = typedValue;
-        }
+		public DoublePropertyViewModel(string name, double typedValue, string unit, IAmlProvider provider)
+			: base(name, unit, provider)
+		{
+			Value = typedValue;
+		}
 	}
 }
