1e4b894f3571598eb3a23e590e41976e714e6c92
Menno Esen <menno.esen@web.de>
2020-06-23 09:50:22 +0200

    added new external data connector to set urls in amlx containers

---
+++ Aml.ViewModel/ViewModels/ExternalGeometryConnectorViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Aml.Contracts;
+using Aml.Engine.CAEX;
+using System;
+
+namespace Aml.ViewModel.ViewModels
+{
+	public class ExternalGeometryConnectorViewModel : ExternalDataConnectorViewModel
+	{
+		internal const string ColladaClassPath = "AutomationMLInterfaceClassLib/AutomationMLBaseInterface/ExternalDataConnector/COLLADAInterfaceUrl";
+		internal const string GenericGeometryClassPath = "AutomationMLInterfaceClassLib/AutomationMLBaseInterface/ExternalDataConnector/ExternalGeometryConnector";
+
+		private const string MimeTypeName = "mimeType";
+
+		public ExternalGeometryConnectorViewModel(IAmlProvider provider, string url) : base(provider)
+		{
+			Location = new Uri(url, UriKind.RelativeOrAbsolute);
+		}
+
+		public ExternalGeometryConnectorViewModel(ExternalInterfaceType model, IAmlProvider provider, string url) : base(model, provider)
+		{
+			Location = new Uri(url, UriKind.RelativeOrAbsolute);
+		}
+
+		public string MimeType
+		{
+			get
+			{
+				var attribute = GetAttribute(MimeTypeName);
+				return attribute == null ? null : attribute.Value;
+			}
+			set
+			{
+				var attribute = GetAttribute(MimeTypeName, true);
+				attribute.Name = MimeTypeName;
+				attribute.AttributeDataType = XMLDataTypeMapper.GetXmlDataType(typeof(string));
+				attribute.Value = value;
+
+				UpdateExternalInterface(value);
+			}
+		}
+
+		private void UpdateExternalInterface(string mimeType)
+		{
+			if (mimeType == XMLMimeTypeMapper.GetMimeType(".dae"))
+			{
+				// COLLADA interface
+				((ExternalInterfaceType)CaexObject).RefBaseClassPath = ColladaClassPath;
+			}
+			else
+			{
+				// Generic class path
+				((ExternalInterfaceType)CaexObject).RefBaseClassPath = GenericGeometryClassPath;
+			}
+		}
+	}
+}
---
+++ Aml.ViewModel/ViewModels/GeometryDataConnectorViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Aml.Contracts;
+using Aml.Engine.CAEX;
+
+namespace Aml.ViewModel
+{
+	/// <summary>
+	/// Defines an interface to a geometry location.
+	/// </summary>
+	public class GeometryDataConnectorViewModel : ExternalDataConnectorViewModel
+	{
+		internal const string ColladaClassPath = "AutomationMLInterfaceClassLib/AutomationMLBaseInterface/ExternalDataConnector/COLLADAInterface";
+		internal const string GenericGeometryClassPath = "AutomationMLInterfaceClassLib/AutomationMLBaseInterface/ExternalDataConnector/GeometryDataConnector";
+
+		private const string MimeTypeName = "mimeType";
+
+		public string MimeType
+		{
+			get
+			{
+				var attribute = GetAttribute(MimeTypeName);
+				return attribute == null ? null : attribute.Value;
+			}
+			set
+			{
+				var attribute = GetAttribute(MimeTypeName, true);
+				attribute.Name = MimeTypeName;
+				attribute.AttributeDataType = XMLDataTypeMapper.GetXmlDataType(typeof(string));
+				attribute.Value = value;
+
+				UpdateExternalInterface(value);
+			} 
+		}
+
+		public GeometryDataConnectorViewModel(IAmlProvider provider) : base(provider)
+		{
+		}
+
+		public GeometryDataConnectorViewModel(ExternalInterfaceType model, IAmlProvider provider) : base(model, provider)
+		{ }
+
+		private void UpdateExternalInterface(string mimeType)
+		{
+			if (mimeType == XMLMimeTypeMapper.GetMimeType(".dae"))
+			{
+				// COLLADA interface
+				((ExternalInterfaceType) CaexObject).RefBaseClassPath = ColladaClassPath;
+			}
+			else
+			{
+				// Generic class path
+				((ExternalInterfaceType)CaexObject).RefBaseClassPath = GenericGeometryClassPath;
+			}
+		}
+	}
+}
